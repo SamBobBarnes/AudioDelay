e11033bf9689059facfbde0c93536dd6cf635108
SamBobBarnes <samuelrbarnes@outlook.com>
2024-03-30 12:29:52 -0500

    init

---
+++ AudioDelay/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+StringBuilder builder = new();
+builder.AppendLine("The following arguments are passed:");
+
+// Display the command line arguments using the args variable.
+foreach (var arg in args)
+{
+    builder.AppendLine($"Argument={arg}");
+}
+
+Console.WriteLine(builder.ToString());
+
+// Return a success code.
+return 0;
