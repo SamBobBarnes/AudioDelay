247c31e8be7103e48dd49de91175dd5f95d9efb6
SamBobBarnes <samuelrbarnes@outlook.com>
2024-04-12 04:46:13 -0500

    linux audio recorder

---
+++ AudioDelay/AudioRecorder/LinuxAudioRecorder.cs
@@ -0,0 +1,30 @@
+﻿using AudioDelay.Args;
+
+namespace AudioDelay.AudioRecorder;
+
+public class LinuxAudioRecorder: AudioRecorder
+{
+    public LinuxAudioRecorder(Arguments args) : base(args)
+    {
+    }
+
+    public override void Play()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void StopPlayback()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Stop()
+    {
+        throw new NotImplementedException();
+    }
+}
