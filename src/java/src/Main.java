4350d11ca7f2ec42af50a9cf5bfe780d849bd5df
sb17057 <sbarnes@vermeer.com>
2024-05-06 09:08:10 -0500

    working in console

--- src/java/src/Main.java
+++ src/java/src/Main.java
@@ -8,26 +8,19 @@ import java.awt.event.WindowEvent;
 
 public class Main {
     public static void main(String[] args) {
+        console();
+//        window();
+    }
+
+    private static void console() {
         CapturePlayback capturePlayback = new CapturePlayback();
-//        JFrame f = new JFrame("Capture/Playback");
-//        f.addWindowListener(new WindowAdapter() {
-//            public void windowClosing(WindowEvent e) { System.exit(0); }
-//        });
-//        f.getContentPane().add("Center", capturePlayback);
-//        f.pack();
-//        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-//        int w = 720;
-//        int h = 340;
-//        f.setLocation(screenSize.width/2 - w/2, screenSize.height/2 - h/2);
-//        f.setSize(w, h);
-//        f.show();
 
         capturePlayback.record();
         System.out.println("recording...");
 
         //delay 5 seconds
         try {
-            Thread.sleep(1000);
+            Thread.sleep(10000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -35,8 +28,28 @@ public class Main {
 
         System.out.println("recording stopped");
         capturePlayback.stopRecording();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("Playing...");
         capturePlayback.playback();
-        System.out.println("Playback stopped.");
+    }
+
+    private static void window() {
+        CapturePlayback capturePlayback = new CapturePlayback();
+        JFrame f = new JFrame("Capture/Playback");
+        f.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) { System.exit(0); }
+        });
+        f.getContentPane().add("Center", capturePlayback);
+        f.pack();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int w = 720;
+        int h = 340;
+        f.setLocation(screenSize.width/2 - w/2, screenSize.height/2 - h/2);
+        f.setSize(w, h);
+        f.show();
     }
 }
