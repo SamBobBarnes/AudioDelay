a2713ec7c6ddb7c205aa6685cf5c7ce604504b78
SamBobBarnes <samuelrbarnes@outlook.com>
2024-06-22 16:10:29 -0500

    added debug logs

--- AudioDelay/Helpers/DelayHandler.cs
+++ AudioDelay/Helpers/DelayHandler.cs
@@ -1,33 +1,28 @@
-﻿namespace AudioDelay.Helpers;
+﻿using Serilog;
+
+namespace AudioDelay.Helpers;
 
 public class DelayHandler(IThreadHandler threadHandler)
 {
-    public void Wait(int ms, bool debug)
+  public void Wait(int ms, bool debug)
+  {
+    if (debug)
     {
-        if (debug)
-        {
-            Console.WriteLine($"Waiting for {ms} milliseconds...");
+      Log.Debug($"Waiting for {ms} milliseconds...");
 
-
-            for (int temp = 0; temp < ms; temp += 1000)
-            {
-                if (ms - temp < 1000)
-                {
-                    threadHandler.Sleep(ms - temp);
-                }
-                else
-                {
-                    threadHandler.Sleep(1000);
-                }
-
-                Console.WriteLine($"Waited for {(double)temp / 1000 + 1} seconds...");
-            }
-
-            Console.WriteLine("Done waiting.");
-        }
+      for (var temp = 0; temp < ms; temp += 1000)
+      {
+        if (ms - temp < 1000)
+          threadHandler.Sleep(ms - temp);
         else
-        {
-            threadHandler.Sleep(ms);
-        }
+          threadHandler.Sleep(1000);
+
+        Log.Debug($"Waited for {(double)temp / 1000 + 1} seconds...");
+      }
+
+      Log.Debug("Done waiting.");
     }
+    else
+      threadHandler.Sleep(ms);
+  }
 }
