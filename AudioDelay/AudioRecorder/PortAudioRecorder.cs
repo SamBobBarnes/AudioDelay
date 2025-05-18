e6966ecdf0a6a1b13a6063d1bcfa5f6291cdaffb
SamBobBarnes <samuelrbarnes@outlook.com>
2025-05-18 13:59:52 -0500

    install PortAudioRecorder.cs

---
+++ AudioDelay/AudioRecorder/PortAudioRecorder.cs
@@ -0,0 +1,28 @@
+﻿using AudioDelay.Args;
+
+namespace AudioDelay.AudioRecorder;
+
+public class PortAudioRecorder : AudioRecorder
+{
+  public PortAudioRecorder(Arguments args) : base(args) { }
+
+  public override void Play()
+  {
+    throw new NotImplementedException();
+  }
+
+  public override void StopPlayback()
+  {
+    throw new NotImplementedException();
+  }
+
+  public override void Start()
+  {
+    throw new NotImplementedException();
+  }
+
+  public override void Stop()
+  {
+    throw new NotImplementedException();
+  }
+}
